484971f5bbf558c22393bbfae282557bf9ce2644
csruiliu <rui.liu@gmx.com>
2023-04-11 12:22:14 -0500

    significantly revise ratchet-duckdb demo

--- ratchet/demo.py
+++ ratchet/demo.py
@@ -2,38 +2,59 @@ import duckdb
 import argparse
 import time
 
-from tpch.queries import *
-
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-q", "--query_name", type=str, action="store",
-                        choices=['q1', 'q2', 'q3', 'q4', 'q5', 'q6', 'q7', 'q8', 'q9', 'q10', 'q11', 'q12', 'q13',
-                                 'q14', 'q15', 'q16', 'q17', 'q18', 'q19', 'q20', 'q21', 'q22', 'slim', 'mid'],
+    parser.add_argument("-q", "--query_name", type=str, action="store", required=True,
+                        choices=['slim', 'mid', "three-way-join"],
                         help="indicate the query id")
     parser.add_argument("-td", "--thread", type=int, action="store", default=1,
                         help="indicate the number of threads in DuckDB")
-    parser.add_argument("-sf", "--scale_factor", type=int, action="store",
-                        help="indicate scale factor of the dataset")
-    parser.add_argument("-pt", "--pause_time", type=int, action="store", default=100,
+    parser.add_argument("-st", "--suspend_time", type=int, action="store",
+                        help="indicate pause time point (second)")
+    parser.add_argument("-sp", "--suspend_probability", type=float, action="store",
                         help="indicate pause time point (second)")
     args = parser.parse_args()
 
     qid = args.query_name
     thread = args.thread
-    suspend_time = args.pause_time
-    sf = "SF" + str(args.scale_factor)
+    suspend_time = args.suspend_time
+    suspend_prob = args.suspend_probability
 
+    # open and connect a database
+    # db_conn = duckdb.connect(database=':memory:')
+    db_conn = duckdb.connect(database='demo.db')
+    db_conn.execute(f"PRAGMA threads={thread}")
+
+    # Copy the TPC-H Datasets
+    tpch_table_names = ["part", "supplier", "partsupp", "customer", "orders", "lineitem", "nation", "region"]
+    tpch_dataset_path = "tpch/dataset/parquet/sf1"
+    for t in tpch_table_names:
+        db_conn.execute(f"CREATE TABLE IF NOT EXISTS {t} AS SELECT * FROM read_parquet('{tpch_dataset_path}/{t}.parquet');")
+
+    # start the query execution
     if qid == "slim":
         exec_query = f"""
             SELECT  avg(L_DISCOUNT) as AVG_DISC,
                     sum(L_QUANTITY) as SUM_QTY
-            FROM    'TPCH_DATAPATH/lineitem.parquet'
+            FROM    lineitem
         """
     elif qid == "mid":
         exec_query = f"""
             SELECT  count(L_EXTENDEDPRICE) AS REVENUE
-            FROM  	'TPCH_DATAPATH/customer.parquet',
+            FROM  	customer,
+                    orders,
+                    lineitem,
+            WHERE	C_CUSTKEY = O_CUSTKEY
+                    AND	L_ORDERKEY = O_ORDERKEY
+                    AND CAST(O_ORDERDATE AS DATE) >= '1994-01-01'
+                    AND CAST(O_ORDERDATE AS DATE) < '1995-01-01'
+        """
+    elif qid == "three-way-join":
+        exec_query = f"""
+            SELECT  count(L_EXTENDEDPRICE) AS REVENUE
+            FROM  	customer,
+            JOIN    ON 
                     'TPCH_DATAPATH/orders.parquet',
                     'TPCH_DATAPATH/lineitem.parquet',
             WHERE	C_CUSTKEY = O_CUSTKEY
@@ -42,26 +63,14 @@ def main():
                     AND CAST(O_ORDERDATE AS DATE) < '1995-01-01'
         """
     else:
-        exec_query = globals()[qid].query
+        raise ValueError("Query is not supported in demo")
 
+    # start the query execution and count the time
     start = time.perf_counter()
-    # db_conn = duckdb.connect(database=':memory:', config={"threads": 2})
-    db_conn = duckdb.connect(database=':memory:')
-    db_conn.execute(f"PRAGMA threads={thread}")
-
-    results = None
-    query_len = len(exec_query) - 1
-    if isinstance(exec_query, list):
-        for idx, query in enumerate(exec_query):
-            if idx == query_len:
-                results = db_conn.execute(query.replace("TPCH_DATAPATH", f"tpch/parquet/{sf}")).fetchdf()
-            else:
-                db_conn.execute(query.replace("TPCH_DATAPATH", f"tpch/parquet/{sf}"))
+    if suspend_time is None:
+        results = db_conn.execute(exec_query).fetchdf()
     else:
-        exec_query = exec_query.replace("TPCH_DATAPATH", f"tpch/parquet/{sf}")
-        results = db_conn.execute_ratchet(exec_query, suspend_time).fetchdf()
-        # results = db_conn.execute(exec_query).fetchdf()
-
+        results = db_conn.execute_ratchet(exec_query, suspend_time, suspend_prob).fetchdf()
     print(results)
     end = time.perf_counter()
     print("Total Runtime: {}".format(end - start))
