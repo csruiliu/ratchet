835bb4656bafe159cb952ba57b366f81b7230817
csruiliu <rui.liu@proton.me>
2023-10-01 14:33:33 -0400

    show checkpoint time

--- src/execution/operator/aggregate/physical_ungrouped_aggregate.cpp
+++ src/execution/operator/aggregate/physical_ungrouped_aggregate.cpp
@@ -524,7 +524,7 @@ SinkFinalizeType PhysicalUngroupedAggregate::Finalize(Pipeline &pipeline, Event 
     if (global_suspend) {
         std::chrono::steady_clock::time_point suspend_check = std::chrono::steady_clock::now();
         uint64_t time_dur_ms = std::chrono::duration_cast<std::chrono::milliseconds>(suspend_check - global_start).count();
-
+        std::cout << "time_dur_ms: " << time_dur_ms << std::endl;
         if (time_dur_ms > global_suspend_point_ms) {
             global_suspend_start = true;
 #if RATCHET_PRINT >= 1
