10f50a2218f3a0dfaf0118f634251a10877c7cbe
csruiliu <rui.liu@proton.me>
2023-10-01 14:57:04 -0400

    show suspension time

--- src/execution/operator/join/physical_hash_join.cpp
+++ src/execution/operator/join/physical_hash_join.cpp
@@ -695,6 +695,7 @@ SinkFinalizeType PhysicalHashJoin::Finalize(Pipeline &pipeline, Event &event, Cl
         std::chrono::steady_clock::time_point suspend_check = std::chrono::steady_clock::now();
         uint64_t time_dur_ms = std::chrono::duration_cast<std::chrono::milliseconds>(suspend_check - global_start).count();
         std::cout << "time_dur_ms: " << time_dur_ms << std::endl;
+        std::cout << "global_suspend_point_ms: " << global_suspend_point_ms << std::endl;
         if (time_dur_ms > global_suspend_point_ms) {
             global_suspend_start = true;
             for (auto &local_ht : sink.local_hash_tables) {
