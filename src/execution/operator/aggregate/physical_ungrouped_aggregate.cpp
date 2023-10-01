d7d5f4b8e668a3541013027a1216ccd40eb09e28
csruiliu <rui.liu@proton.me>
2023-10-01 15:12:20 -0400

    update time count

--- src/execution/operator/aggregate/physical_ungrouped_aggregate.cpp
+++ src/execution/operator/aggregate/physical_ungrouped_aggregate.cpp
@@ -523,7 +523,7 @@ SinkFinalizeType PhysicalUngroupedAggregate::Finalize(Pipeline &pipeline, Event 
 
     if (global_suspend) {
         std::chrono::steady_clock::time_point suspend_check = std::chrono::steady_clock::now();
-        uint64_t time_dur_ms = std::chrono::duration_cast<std::chrono::milliseconds>(suspend_check - global_start).count();
+        uint64_t time_dur_ms = std::chrono::duration_cast<std::chrono::microseconds>(suspend_check - global_start).count() / 1000;
         std::cout << "time_dur_ms: " << time_dur_ms << std::endl;
         std::cout << "global_suspend_point_ms: " << global_suspend_point_ms << std::endl;
         if (time_dur_ms > global_suspend_point_ms) {
